be7b104478534544149e6099e93c7bdc66bd94cb
dhancode46 <wirawardhana49@gmail.com>
2021-10-28 11:21:57 +0700

    update 1-hello on functions

--- functions/1-hello.js
+++ functions/1-hello.js
@@ -4,6 +4,6 @@ exports.handler = async (event, context) => {
   // always return object
   return {
     statusCode: 200,
-    body: 'Our first netlify serverless functions',
+    body: 'Hello, this first my serverless functions',
   };
 };
