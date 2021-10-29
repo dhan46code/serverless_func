6970d976c087b4aea5c2d762e52424ff93e262c0
dhancode46 <wirawardhana49@gmail.com>
2021-10-29 09:18:54 +0700

    cors fix with add headers

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -2,9 +2,9 @@ const items = require('../assets/data');
 
 exports.handler = async (event, context) => {
   return {
-    headers : {
-      'Access-Control:Allow-Origin' : '*'
-    }
+    headers: {
+      'Access-Control:Allow-Origin': '*',
+    },
     statusCode: 200,
     body: JSON.stringify(items),
   };
